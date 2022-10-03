611302e7dd28a2249a066d541f64835d03520597
gaikwadsomeshwar <gaikwadsomeshwar6@gmail.com>
2022-10-03 12:15:47 +0530

    Add funtionality to hide Add New ToDo while Editing existing Task

--- client/src/components/ToDo.js
+++ client/src/components/ToDo.js
@@ -1,16 +1,18 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import AddNewToDo from "./AddNewToDo";
 import TodoList from "./TodoList";
 
 function Todo() {
   const [todos, setTodos] = useState([]);
+  const [edit, setEdit] = useState(false);
 
   return (
     <>
       <div className="todo">
-        <AddNewToDo todos={[todos, setTodos]} />
-        <TodoList todos={[todos, setTodos]}  />
+        {!edit && (
+          <AddNewToDo todos={[todos, setTodos]} edits={[edit, setEdit]} />
+        )}
+        <TodoList todos={[todos, setTodos]} edits={[edit, setEdit]} />
       </div>
     </>
   );
--- client/src/components/ToDoCard.js
+++ client/src/components/ToDoCard.js
@@ -1,12 +1,10 @@
 import React, { useState } from "react";
 import { TiEdit } from "react-icons/ti";
-import { RiCloseCircleLine } from "react-icons/ri";
-import { RiCheckboxCircleLine } from "react-icons/ri";
+import { RiCloseCircleLine, RiCheckboxCircleLine } from "react-icons/ri";
 import EditTodo from "./EditTodo";
 
-function ToDoCard({ task,  list, setTodos }) {
+function ToDoCard({ task, list, setTodos, edits }) {
   const [edit, setEdit] = useState(false);
-
   const completeTodo = (id) => {
     const updatedTodo = list.map((t) => {
       if (t.id === id) {
@@ -25,13 +23,18 @@ function ToDoCard({ task,  list, setTodos }) {
   return (
     <>
       {edit ? (
-        <EditTodo currentTodo={task} list={list} setEdit={setEdit} />
-      ) : (task.isComplete || false )? (
+        <EditTodo
+          currentTodo={task}
+          list={list}
+          setEdit={setEdit}
+          edits={edits}
+        />
+      ) : task.isComplete ? (
         <div className="todo-row complete">
           <p className="todo-text"> {task.value}</p>
-          
+
           <div className="icons">
-          <RiCloseCircleLine
+            <RiCloseCircleLine
               className="delete-icon"
               onClick={() => deleteTodo(task.id)}
             />
@@ -41,7 +44,14 @@ function ToDoCard({ task,  list, setTodos }) {
         <div className="todo-row ">
           <p className="todo-text"> {task.value}</p>
           <div className="icons">
-            <TiEdit onClick={() => setEdit(true)} className="edit-icon" />
+            <TiEdit
+              onClick={() => {
+                setEdit(true);
+                const [editing, setEditing] = edits;
+                setEditing(true);
+              }}
+              className="edit-icon"
+            />
             <RiCheckboxCircleLine
               className="complete-icon"
               onClick={() => completeTodo(task.id)}
