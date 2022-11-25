2ffe1c5eb054c5184a1cd93da7d4c125ca340b7c
Ajay Singh <ajay_singh6@persistent.com>
2022-11-25 18:10:27 +0530

    todo card added

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
 import Todo from "./components/ToDo";
 import Formcontainer from "./components/Formcontainer";
+import Navbar from "./components/Navbar";
+import Todolist from "./components/Todolist";
 
 function App() {
   return (
     <>
-
-    <Formcontainer/>
+    <Navbar></Navbar>
+    <Todolist/>
+    {/* <Formcontainer/> */}
       {/* <div className="todo-app">
         <h1 className="todo-heading"> Write Your Task Here.</h1>
 
