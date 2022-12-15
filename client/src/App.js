f072613c2e4f572f4595c9a26230cb6cfeb38f53
staykirak <omar.farooq023@gmail.com>
2022-12-15 12:37:43 +0530

    Revert "temp changes"
    This reverts commit cf42e60124666bca0c323b086afeb0aefa6c3d10.

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import Todo from "./components/ToDo";
 
 import Navbar from "./components/Navbar";
-import Formcontainer from "./components/Formcontainer";
 import Todolist from "./components/TodoList";
 import { createContext, useState } from "react";
 import {
@@ -33,9 +32,9 @@ function App() {
   return (
 
     <div className="main">
-      {/* <Navbar />
-      <Todo /> */}
-      <BrowserRouter>
+      <Navbar />
+      <Todo />
+      {/* <BrowserRouter>
         <Routes>
           <Route path="/" element={<Todolist />}>
             {" "}
@@ -47,7 +46,7 @@ function App() {
             {" "}
           </Route>
         </Routes>
-      </BrowserRouter>
+      </BrowserRouter> */}
       <UserContext.Provider value={{user,setUser}}>
 
       <RouterProvider router={router}></RouterProvider>
